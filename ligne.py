03365ae73970e63146b08d5fe9ca4bdaf1718554
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2011-04-04 17:49:42 +0200

    more tries

--- ligne.py
+++ ligne.py
@@ -12,22 +12,9 @@ win._refreshThreshold=1/50.0+0.004 #i've got 50Hz monitor and want to allow 4ms 
 #set the log module to report warnings to the std output window (default is errors only)
 log.console.setLevel(log.WARNING)
 
-#fixSpot = visual.PatchStim(win,tex="none", mask="gauss",
-#        pos=(0,0), size=(50,50),color='red', autoLog=False)
-#
-#import scipy
-# noiseTexture = np.array(Image.open('/Applications/PsychoPy2.app/Contents/Resources/lib/python2.6/psychopy/demos/coder/face.jpg'))
-# noiseTexture /= np.max(np.abs(noiseTexture))*2 -1
-downscale=2
-#noiseTexture = scipy.random.rand(512/2**downscale,512/2**downscale)*2.0-1
-#index_array = np.arange(noiseTexture.size)
-#noise = visual.PatchStim(win, tex=noiseTexture, 
-#    pos=(0.0,0), size=(1900,1200), units='pix',
-#    opacity=1.,
-#    interpolate=False)
 
-#filename = '/Volumes/tera/10-12-31_RTC/10-03-23_cnprs/spip/prive/vignettes/deb.png'
-#signalTexture = scipy.random.rand(64/2**downscale,64/2**downscale)*2.0-1
+downscale=2
+
 signalTexture = np.zeros((512/2**downscale,512/2**downscale))
 signalTexture[:, 512/2**downscale/2] = 1.
 signal_index_array = np.arange(signalTexture.size)
@@ -37,15 +24,10 @@ stimulus = visual.PatchStim(win,tex=signalTexture,#'/Applications/PsychoPy2.app/
     opacity=1.,
     )
 
-#stimulus = visual.SimpleImageStim(win,image='/Applications/PsychoPy2.app/Contents/Resources/lib/python2.6/psychopy/demos/coder/face.jpg',
-#    pos=(0.0,0.0), units ='norm', contrast =.3
-#    )
-
 myMouse = event.Mouse(win=win)
 message = visual.TextStim(win, pos=(-900,-450), alignHoriz='left', height=40, autoLog=False, color = (0,0,1))
 
 position, position_Increment = 2.1, 0.02
-
 
 import socket
 
@@ -58,10 +40,10 @@ b= socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 addr =(host,port)
 print addr
 b.bind(addr)
-b.settimeout(5)
+b.settimeout(0)
 
 
-t=lastFPSupdate=lastT=0
+t=lastFPSupdate=0
 while True:
     t=globalClock.getTime()
 #    print  win.fps(), str(win.fps())
@@ -73,53 +55,27 @@ while True:
         if key in ['escape','q']:
             core.quit()
 
-    wheel_dX, wheel_dY = myMouse.getWheelRel()
-    position += wheel_dY*position_Increment
-    event.clearEvents()#get rid of other, unprocessed events
-    
-#    if True:#t-lastT>.05:
-#        lastT=t
-#        np.random.shuffle(index_array)
-#    
-#    noise.setTex(noiseTexture.ravel()[index_array].reshape(noiseTexture.shape))
-#    noise.draw()
-
     try :
         dat = b.recvfrom(1024)
         position = float(dat[0])
         print position
+        message.setText(str(int(win.fps()))+  " fps / " +  str(position) + " / [Esc] to quit" )
     except:
-        print ("nodata")
-#	else :
-#		print float(dat[0])
-
+        pass
+#        print ("nodata")
+    else:
+        print float(dat[0])
 
     x =  position / 3.5 *2 -1 #t*speed%2 -1 # relative coordinate between -1 and 1
     x = np.int( (x+1)/2 *128 )/128. *2 - 1 # taking the integer part according to the texture's scale
     x = x*1900/2 # scaling to pix units
-#
-#    if abs(abs(x)-1900/2)<300: 
-#        np.random.shuffle(signal_index_array)
-#        stimulus.setTex(signalTexture.ravel()[signal_index_array].reshape(signalTexture.shape))
 
     stimulus.setPos((x, 0))#32*1200 / 128))
     stimulus.draw()
-#    
-#    if abs(abs(x)-400)<20: 
-#        fixSpot.setPos((np.sign(x)*800, -400.))
-#        fixSpot.draw()
-#        
-#    fixSpot.setPos((x,-400))
-#    fixSpot.draw()
-        
-        
+
     message.draw()
 
     win.flip()
-import pylab
-pylab.plot(win.frameIntervals)
-pylab.show()
-win.saveFrameIntervals(fileName=None, clear=True)
 
 win.close()
 
