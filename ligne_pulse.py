d6beaa5ce74bc034947254d53842560a398efa5e
Laurent Perrinet <laurent.perrinet@incm.cnrs-mrs.fr>
2011-04-06 15:04:59 +0200

    cleaning up lines

--- ligne_pulse.py
+++ ligne_pulse.py
@@ -7,7 +7,7 @@ width, width_Increment = 1,  1 # largeur de la ligne en pixels
 n_line, decalage, decalage_increment = 3, 10, 1
 freq, freq_Increment = .01, 0.001 # largeur de la ligne en pixels
 freqx, freqx_Increment = 10.0, 0.1 # largeur de la ligne en pixels
-
+size_h, size_h_increment = 1., .02
 ##########################################
 from psychopy import visual, event, core#, log
 import numpy as np
@@ -17,16 +17,25 @@ win = visual.Window(fullscr=True, color=[-1,-1,-1] , units='norm')
 win.setRecordFrameIntervals(True)
 win._refreshThreshold=1/20.0+0.004 #i've got 50Hz monitor and want to allow 4ms tolerance
 
-# TODO : def texture():
 signalTexture = -np.ones((512/2**downscale,512/2**downscale))
-
-size_line = signalTexture.shape[0]
-signal_index_array = np.arange(signalTexture.size)
-stimulus = visual.PatchStim(win,tex=signalTexture,#'/Applications/PsychoPy2.app/Contents/Resources/lib/python2.6/psychopy/demos/coder/face.jpg',
-#    pos=(0.0,0.0),
-    size=(1900,1200), units='pix',
+stimulus = visual.PatchStim(win,tex=signalTexture,
+    size=(size_h, size_h), units='height',
     opacity=1.,
     )
+signal_index_array = np.arange(signalTexture.size)
+size_line = signalTexture.shape[0]
+
+def texture(freqx, freq, width, decalage, n_line, t):
+    signalTexture = -np.ones((512/2**downscale,512/2**downscale))
+    middle = 512/2**downscale/2
+    pulse = np.sin( 2*np.pi * (freqx * np.linspace(0, 1 , size_line) + t*freq*360.0))
+    for i_line in range(n_line):
+        signalTexture[:, middle-width + i_line*decalage] = pulse
+    return signalTexture
+
+signalTexture = texture(freqx, freq, width, decalage, n_line, 0.)
+stimulus.setTex(signalTexture.ravel()[signal_index_array].reshape(signalTexture.shape))
+
 
 showText = True
 myMouse = event.Mouse(win=win)
@@ -49,6 +58,12 @@ while True:
     for key in event.getKeys():
         if key in ['s']:
             showText = not(showText)
+        elif key in ['x']:
+            size_h += size_h_increment
+            stimulus.setSize((size_h, size_h))
+        elif key in ['c']:
+            size_h += -size_h_increment
+            stimulus.setSize((size_h, size_h))
         elif key in ['d']:
             decalage += decalage_increment
         elif key in ['f']:
@@ -63,16 +78,10 @@ while True:
     if width==0: width=1
     event.clearEvents() # get rid of other, unprocessed events
 
-    # if not(wheel_dX==0): # or (key in ['d']) or (key in ['f']): 
-    signalTexture = -np.ones((512/2**downscale,512/2**downscale))
-    middle = 512/2**downscale/2
-    for i_line in range(n_line):
-        pulse = np.sin( 2*np.pi * (freqx * np.linspace(0, 1 , size_line) + t*freq*360.0))
-        signalTexture[:, (middle + i_line*decalage)] = pulse
-        # TODO : wtf?
+    signalTexture = texture(freqx, freq, width, decalage, n_line, t)
     stimulus.setTex(signalTexture.ravel()[signal_index_array].reshape(signalTexture.shape))
 
-    stimulus.setPos((X*1200, Y*900), operation = '', units = 'norm')
+    stimulus.setPos((X, Y), operation = '')
     stimulus.setOri(t*rotspeed*360.0)
     stimulus.draw()
     message.draw()
