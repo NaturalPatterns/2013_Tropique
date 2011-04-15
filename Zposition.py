fe7fa8c1b698959edbbab880cb0953e9db583b96
Laurent Perrinet <laurent.perrinet@incm.cnrs-mrs.fr>
2011-04-15 16:37:45 +0100

    la branche de Juju

---
+++ Zposition.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+import freenect
+import signal
+from calibkinect import depth2xyzuv, xyz_matrix
+import os
+import socket
+
+image_depth = None
+keep_running = True
+
+import numpy as np
+
+depth_shape=(640,480)
+depth_min, depth_max= 0., 3.5
+N_hist = 2**8 
+max_depth = 3.5 # in meters
+matname = 'depth_map.npy'
+depth_hist = np.load(matname)    
+
+i_frame = 0
+
+def display_depth(dev, data, timestamp, display=False):
+    """
+    
+    Args:
+    index: Kinect device index (default: 0)
+    format: Depth format (default: DEPTH_11BIT)
+    
+    Returns:
+    (depth, timestamp) or None on error
+    depth: A numpy array, shape:(640,480) dtype:np.uint16
+    timestamp: int representing the time
+
+    """
+    global image_depth, i_frame, depth_hist
+    # low-level segmentation
+    # from http://nicolas.burrus.name/index.php/Research/KinectCalibration
+    Z = 1.0 / (data * -0.0030711016 + 3.3309495161)
+    shadows = Z > depth_max # irrelevant calculations
+    shadows += Z < depth_min # irrelevant calculations
+    Z = Z * (1-shadows) + depth_max * shadows
+    score = (depth_hist[:, :, 0] - Z) / (np.sqrt(depth_hist[:, :, 1]) + .5*np.sqrt(depth_hist[:, :, 1]).mean()) 
+    score = score * (1-shadows)  - 10. * shadows
+    ROI = score > 0.
+    #print Z.mean()
+    if np.sum(ROI) > 0:
+        Z_mean = np.sum(Z*ROI) / np.sum(ROI)    
+        print Z_mean
+	s.sendto(str(Z_mean),addr)
+	#s.sendto(str(Z_mean),addr2)
+	print ("datasend = ", Z_mean, addr)
+        
+def handler(signum, frame):
+    global keep_running
+    keep_running = False
+
+def body(*args):
+    if not keep_running:
+        raise freenect.Kill    
+    
+def main():
+    global depth_hist, record_list, record
+    print('Press Ctrl-C in terminal to stop')
+    signal.signal(signal.SIGINT, handler)
+    freenect.runloop(depth=display_depth,
+                     body=body)
+    
+    if record:
+        os.system('ffmpeg -v 0 -y  -f image2  -sameq -i _frame%03d.png  ' + record + ' 2>/dev/null')
+        for fname in record_list: os.remove(fname)
+
+#description res
+host = '192.168.1.4'
+host2 = '192.168.1.3'
+port = 3002
+s= socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+addr =(host,port)
+addr2 = (host2,port)
+print addr; addr2
+
+if __name__ == "__main__":
+    main()
+
---
+++ send_position_spe_bb.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+"""
+    Script de test de la Kinect pour extraire la position 3D
+    
+"""
+# paramètres variables #
+verbose=True
+depth_min, depth_max= 0., 4.5
+N_frame = 100 # time to learn the depth map
+tilt = 0 # vertical tilt of the kinect
+N_hist = 2**8 
+threshold = 3.5
+downscale = 4
+smoothing = 1.5
+noise_level = .8
+# paramètres fixes #
+depth_shape=(640,480)
+matname = 'depth_map.npy'
+i_frame = 0
+record_list = []
+image_depth = None
+keep_running = True
+start = True
+prof_m, az_m, el_m = depth_max, 0. , 0.
+#################################################
+import freenect
+import signal
+#import frame_convert
+from calibkinect import depth2xyzuv, xyz_matrix
+import os
+import numpy as np
+depth_hist = np.load(matname)    
+import scipy.ndimage as nd
+import socket
+#################################################
+def display_depth(dev, data, timestamp, verbose=verbose):
+    """
+    
+    Args:
+    index: Kinect device index (default: 0)
+    format: Depth format (default: DEPTH_11BIT)
+    
+    Returns:
+    (depth, timestamp) or None on error
+    depth: A numpy array, shape:(640,480) dtype:np.uint16
+    timestamp: int representing the time
+
+    """
+    global depth_hist, addrs, prof_m
+    Z = 1.0 / (data[::downscale,::downscale] * -0.0030711016 + 3.3309495161)
+    shadows = Z > depth_max # irrelevant calculations
+    shadows += Z < depth_min # irrelevant calculations
+    Z = Z * (1-shadows) + depth_max * shadows
+    Z = nd.gaussian_filter(Z, smoothing)
+    score = (depth_hist[:, :, 0] - Z)  / ((1.-noise_level)*np.sqrt(depth_hist[:, :, 1]) + noise_level*np.sqrt(depth_hist[:, :, 1]).mean())
+    attention = np.argwhere(score.ravel() > threshold)
+    detect =  (attention.shape[0] > 0)
+    if detect:
+        # computing positions
+        U, V = np.mgrid[:480:downscale,:640:downscale]
+        U_, V_ = U.ravel(), V.ravel()
+        data_ = data[::downscale,::downscale].ravel()
+        xyz, uv = depth2xyzuv(data_[attention], u=U_[attention], v=V_[attention])
+    
+        prof, az, el = -xyz[:,2], -xyz[:,0], -xyz[:,1]
+        prof_m, az_m, el_m = prof.mean(), az.mean(), el.mean()
+    else:
+        prof_m, az_m, el_m = depth_max, depth_max, depth_max
+
+    for addr in addrs:
+        my_array = str(prof_m) + "," + str( az_m) +"," + str( el_m)
+	s.sendto((my_array),addr)
+	if verbose: print ("datasend = ",my_array , addr)
+ 
+
+def handler(signum, frame):
+    global keep_running
+    keep_running = False
+
+def body(dev, ctx):#*args):
+    global keep_running
+#    global image_depth
+    
+    freenect.set_led(dev, 0)
+    freenect.set_tilt_degs(dev, tilt)
+
+
+    if not keep_running:
+        freenect.set_led(dev, 5)
+        raise freenect.Kill
+
+def main():
+    global depth_hist, record_list, record, addrs, s
+    #description res
+    hosts = ['192.168.1.254'] #['192.168.1.4', '192.168.1.3']
+    port = 3002
+    s= socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket.SOCK_DGRAM)
+    addrs = [(host,port) for host in hosts]
+    print addrs
+    
+    print('Press Ctrl-C in terminal to stop')
+    signal.signal(signal.SIGINT, handler)
+    freenect.runloop(depth=display_depth,
+                     body=body)
+    s.close()
+
+if __name__ == "__main__":
+    main()
+
+
