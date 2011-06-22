455f56ec99b881f0899bc8eee28ba9718ab94579
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2011-06-22 14:21:54 +0100

    Tropique : committing bis  tous les changements depuis la version lombric... j'essaie un coup vers le mode te?tard

--- projection/demo-smoke.py
+++ projection/demo-smoke.py
@@ -38,6 +38,7 @@ I = glumpy.Image(Z, interpolation='bicubic', cmap=cmap, vmin=0, vmax=5)
 t, t0, frames = 0,0,0
 
 window = glumpy.Window(800,800)
+window._fullscreen = True
 window.last_drag = None
 
 @window.event
