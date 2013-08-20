f41826a1a0a0c8941acbb85e81954d681c7cc72e
meduz <orez.org@gmail.com>
2013-08-20 11:44:19 +0200

    remontage atelier + concept

---
+++ init/spread_ssh_keys.sh
@@ -0,0 +1,7 @@
+# mac-minis
+ssh-copy-id -i ~/.ssh/id_rsa.pub tropic@10.42.0.51
+ssh-copy-id -i ~/.ssh/id_rsa.pub tropic@10.42.0.52
+ssh-copy-id -i ~/.ssh/id_rsa.pub tropic@10.42.0.53
+ssh-copy-id -i ~/.ssh/id_rsa.pub tropic@10.42.0.54
+ssh-copy-id -i ~/.ssh/id_rsa.pub tropic@10.42.0.55
+ssh-copy-id -i ~/.ssh/id_rsa.pub tropic@10.42.0.56
